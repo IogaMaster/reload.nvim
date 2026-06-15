824752ac6a69d5cd5134f0cb17ed9d3276f0cf19
IogaMaster <iogamastercode@gmail.com>
2026-06-15 10:36:47 -0600

    Make simpler

--- lua/reload.lua
+++ lua/reload.lua
@@ -4,16 +4,13 @@ local config_defaults = {}
 M.config = {}
 function M.reload()
     vim.cmd [[
-        :mksession! /tmp/reload.vim | restart source /tmp/reload.vim
+        :mksession! /tmp/reload.vim | restart silent! source /tmp/reload.vim
     ]]
 end
 
-function M.setup(opts)
-    opts = opts or {}
-    M.config = vim.tbl_deep_extend("force", config_defaults, opts)
-    vim.api.nvim_create_user_command("reload", function()
-        M.reload()
-    end, {})
-end
+-- Ignore setup, it just does a simple command
+vim.api.nvim_create_user_command("Reload", function()
+    M.reload()
+end, {})
 
 return M
